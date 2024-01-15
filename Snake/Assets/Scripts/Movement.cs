fc92a50c0e9ed7aa2a0d0f5c63226b5979af87be
dennis-limontov <dennis.limontov@gmail.com>
2024-01-15 23:53:42 +0300

    Changed snake movement

--- Snake/Assets/Scripts/Movement.cs
+++ Snake/Assets/Scripts/Movement.cs
@@ -8,25 +8,23 @@ namespace Snake
         private InputHandler _inputHandler;
 
         [SerializeField]
-        private float _snakePower;
+        private float _snakeSpeed;
 
         [SerializeField]
         private float _snakeRotationSpeed;
 
         private Rigidbody _snakeHeadRb;
-        private SnakeHead _snake;
+
+        private float _snakeDestinationAngle;
 
         private void Start()
         {
             _snakeHeadRb = GetComponent<Rigidbody>();
-            _snake = _snakeHeadRb.GetComponent<SnakeHead>();
         }
 
         private void FixedUpdate()
         {
-            Vector2 direction = _snakePower * _snake.SnakeLength * _inputHandler.Direction;
-            _snakeHeadRb.AddForce(direction.x, 0f, direction.y, ForceMode.Force);
-            //transform.Rotate(_inputHandler.Direction * _snakeRotationSpeed);
+            _snakeHeadRb.velocity = _snakeSpeed * transform.forward;
         }
 
         private void Update()
@@ -35,7 +33,7 @@ namespace Snake
             float v = _inputHandler.Direction.y;
             if ((h == 0f) && (v == 0f))
             {
-                Vector3 chapa, chipi = transform.localEulerAngles;
+                /*Vector3 chapa, chipi = transform.localEulerAngles;
                 if (chipi.y > 180f)
                 {
                     chapa = new Vector3(0f, 359.999f, 0f);
@@ -44,11 +42,18 @@ namespace Snake
                 {
                     chapa = Vector3.zero;
                 }
-                transform.localEulerAngles = Vector3.Slerp(chipi, chapa, Time.deltaTime * 2f);
+                transform.localEulerAngles = Vector3.Slerp(chipi, chapa, Time.deltaTime * 2f);*/
             }
             else
             {
-                transform.localEulerAngles = new Vector3(0f, Mathf.Atan2(h, v) * 180f / Mathf.PI, 0f);
+                _snakeDestinationAngle = Mathf.Atan2(h, v) * 180f / Mathf.PI;
+                
+                if (Mathf.Abs(transform.localEulerAngles.y - _snakeDestinationAngle) > 0.1f)
+                {
+                    float sign = (_snakeDestinationAngle > transform.localEulerAngles.y) ? 1f : -1f;
+                    transform.Rotate(0f, _snakeRotationSpeed * sign * Time.deltaTime, 0f);
+                }
+                //transform.localEulerAngles = new Vector3(0f, _snakeDestinationAngle, 0f);
             }
         }
     }
